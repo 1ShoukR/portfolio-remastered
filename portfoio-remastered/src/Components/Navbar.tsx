7c45c79e6cfe0de2d8a4d43f3de3a1721bc6fae5
1ShoukR <rahminshoukoohi@gmail.com>
2023-04-09 16:52:11 -0400

    removed unnecessary xmls in a tag

--- portfoio-remastered/src/Components/Navbar.tsx
+++ portfoio-remastered/src/Components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = (props: Props) => {
 			</div>
 			<div className="block lg:hidden">
 				<button className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white">
-					<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<svg className="fill-current h-3 w-3" viewBox="0 0 20 20">
 						<title>Menu</title>
 						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
 					</svg>
