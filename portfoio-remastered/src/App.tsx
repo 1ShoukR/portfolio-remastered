cadc111739a9ba4047c4bc5d7dd3d47b2891dfcd
1ShoukR <rahminshoukoohi@gmail.com>
2023-04-09 16:37:27 -0400

    added browser router support code

--- portfoio-remastered/src/App.tsx
+++ portfoio-remastered/src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from './Components/About'
+import Resume from './Components/Resume'
+import PersonalProjects from './Components/PersonalProjects'
+import ContactMe from './Components/ContactMe'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  )
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/resume" element={<Resume />} />
+				<Route path="/personal-projects" element={<PersonalProjects />} />
+				<Route path="/contact-me" element={<ContactMe />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App
---
+++ portfoio-remastered/src/Components/PersonalProjects.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const PersonalProjects = (props: Props) => {
+  return <div>PersonalProjects</div>;
+};
+
+export default PersonalProjects;
