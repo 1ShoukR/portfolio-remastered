7bd96f406ff0fa5d34098221941035c2fe90e1eb
1ShoukR <rahminshoukoohi@gmail.com>
2023-04-09 17:38:08 -0400

    added mui icons

--- portfoio-remastered/src/Components/Home.tsx
+++ portfoio-remastered/src/Components/Home.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default Home;
