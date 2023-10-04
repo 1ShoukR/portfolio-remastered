8172786c36c26a72f6d4a7c2a2ac3ebf997cedfa
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-10-04 09:31:49 -0400

    removed description from app

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -1,100 +1,39 @@
 import React from 'react';
 import MyPic from '../assets/me.jpg';
-import { Button } from '@mui/material';
-import { Card } from '@mui/material';
-import { Typography } from '@mui/material';
-
 
 type Props = {};
 
 const Description = (props: Props) => {
 	return (
-		<div className="flex align-center mt-5 justify-center items-center">
-			<div className="myPic lg:max-w-full md:flex md:flex-col justify-center items-center sm:flex sm:flex-col justify-center items-center">
-				<Card
-					className="muiCARD"
-					sx={{
-						display: 'flex',
-						marginTop: {
-							xs: '1rem',
-                            md: '0px'
-						},
-						height: {
-							xs: 'auto',
-							sm: 'auto',
-						},
-						width: {
-							xs: '100%',
-							sm: '90%',
-							md: '1000px',
-						},
-						flexDirection: {
-							xs: 'column',
-							sm: 'column',
-							md: 'row',
-						},
-						padding: '40px',
-						borderRadius: '16px',
-						boxShadow: 3,
-						bgcolor: 'text.secondary',
-						gap: '30px',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}>
-					<div className="pictureWrapper">
-						<div className="pictureContainer">
-							<img className="myPicSpecifics w-full max-w-full sm:h-1/2 " src={MyPic} alt="me" />
-						</div>
+		<div className="flex flex-col md:flex-row items-center bg-gray-100 p-6 rounded-lg shadow-lg max-w-3xl md:max-w-5xl mx-auto mt-6">
+			<div className="flex flex-col items-center w-full md:w-1/2 mb-6 md:mb-0">
+				<div className="w-3/4 md:w-full mb-4">
+					<img className="rounded-lg object-cover w-full" src={MyPic} alt="me" />
+				</div>
+				<div className="text-center md:text-left">
+					<a href="https://www.linkedin.com/in/your-profile/" className="text-blue-500 hover:underline">
+						LinkedIn
+					</a>
+					{/* Add more links or text here */}
+				</div>
+			</div>
+			<div className="w-full md:w-1/2 p-4 md:p-0 md:pl-6">
+				<h2 className="text-xl font-bold mb-2">A little about me</h2>
+				<div className="bg-white p-6 rounded-lg shadow-lg">
+					<p className="mb-4">
+						I am incredibly passionate about all things tech! I have attended and graduated the DigitalCrafts Web Development Bootcamp to learn Full-Stack Web Development in order to jumpstart my
+						career in tech, and it is the best decision I have ever made in my life. My love for coding continues to grow exponentially as I learn more and more about the intricacies and nuances of
+						each language that I pursue!
+					</p>
+					<div className="flex space-x-4 mt-4">
+						<a href="/Resume" className="btn btn-outline">
+							Resume
+						</a>
+						<a href="/Contact_Me" className="btn btn-outline">
+							Contact Me
+						</a>
 					</div>
-					<div className="descriptionWrapper">
-						<div className="description">
-							<Typography sx={{ color: '#F0EAD6', fontWeight: 'bold' }} className="aLittleAboutMe">
-								A little about me
-							</Typography>
-							<Typography sx={{ color: '#F0EAD6' }} variant="body1" className="text-white-87">
-								I am incredibly passionate about all things tech! I have attended and graduated the DigitalCrafts Web Development Bootcamp to learn Full-Stack Web Development in order to jumpstart my
-								career in tech, and it is the best decision I have ever made in my life. My love for coding continues to grow exponentially as I learn more and more about the intricacies and nuances
-								of each language that I pursue!
-							</Typography>
-						</div>
-						<div className="buttonWrapper md:px-4 md: mt-6">
-							<div>
-								<a href="/Resume">
-									<Button
-										variant="outlined"
-										sx={{
-											marginTop: {
-												xs: '70px',
-											},
-											padding: {
-												xs: '10px',
-											},
-											color: '#F0EAD6',
-										}}>
-										Resume
-									</Button>
-								</a>
-							</div>
-							<div>
-								<a href="/Contact_Me">
-									<Button
-										variant="outlined"
-										sx={{
-											marginTop: {
-												xs: '70px',
-											},
-											padding: {
-												xs: '10px',
-											},
-											color: '#F0EAD6',
-										}}>
-										Contact Me
-									</Button>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Card>
+				</div>
 			</div>
 		</div>
 	);
